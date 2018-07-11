57ee61224a7dcca3dc7e397c08a2b50a6f8aa8af
bapvn <bapvn@BapVns-MacBook-Pro-2.local>
2018-07-11 16:31:53 +0700

    Update code.

--- app.js
+++ app.js
@@ -29,7 +29,6 @@ io.on('connection', function(socket){
     });
 });
 
-
 //Show form update data
 app.get('/', function (request, response) {
     response.render('index');
@@ -37,22 +36,26 @@ app.get('/', function (request, response) {
 
 const request = require('request');
 const user = require('./account');
-const CHAT_URL = 'https://prototype-chat.robin-aisystem.com';
 const API_URL = 'http://35.200.33.192:8089/api';
 
 const INTERVAL_SECOND = 1000;
 const REQUEST_PER_SECOND = 10;
 
 let TestApi;
-
 (function (TestApi) {
     TestApi.Performance = {
+
+        /**
+         * Bulk access
+         * @param data
+         * @constructor
+         */
         Run: function (data) {
             (async () => {
                 const url = data.url;
                 const method = data.method;
-                const param = data.params;
-                const requestPerSecond = data.request;
+                const param = data.params.data;
+                const requestPerSecond = data.request || 10;
                 const second = data.second;
 
                 let promises = [];
@@ -71,175 +74,73 @@ let TestApi;
                 promises = await Promise.all(promises);
                 user.token = promises[0];
 
-
                 let time = 1;
                 setInterval(async () => {
                     promises = [];
-                    //Status now
-
                     let fail = 0;
                     let pass = 0;
                     for (let i = 0; i < requestPerSecond; i += 1) {
                         promises.push(new Promise((resolve) => {
-                            request.get(`${API_URL}/api/status_now`, {
+                            const body = {
                                 json: true,
+                                data: param,
                                 auth: {
                                     'bearer': user.token
                                 }
-                            }, (err, res, body) => {
-                                if (err)
-                                    fail += 1;
-                                else
-                                    pass += 1;
+                            };
 
-                                if (fail+pass === requestPerSecond-1){
-                                    io.emit('test-api', {
-                                        request:time,
-                                        failed: fail,
-                                        successful: pass+1
+                            switch (method){
+                                case 'get':
+                                    request.get(url, body, (err, res, body) => {
+                                        if (err)
+                                            fail += 1;
+                                        else
+                                            pass += 1;
+                                        TestApi.Performance.SendData(time, pass, fail, requestPerSecond);
+                                        resolve(body);
                                     });
-                                }
-                                resolve(body);
-                            });
+                                break;
+                                case 'post':
+                                    request.post(url, body, (err, res, body) => {
+                                        if (err)
+                                            fail += 1;
+                                        else
+                                            pass += 1;
+                                        TestApi.Performance.SendData(time, pass, fail, requestPerSecond);
+                                        resolve(body);
+                                    });
+                                break;
+                            }
                         }))
-                    }
-
-                    await Promise.all(promises);
+                    }await Promise.all(promises);
                     time += 1;
-                },
-                    INTERVAL_SECOND);
+                }, INTERVAL_SECOND);
                 })().then(() => {
                 console.log('Test finished');
             }).catch((err) => {
                 console.log(err);
             });
+        },
+
+        /**
+         * Emit data to view
+         * @param time
+         * @param pass
+         * @param fail
+         * @param total_request
+         * @constructor
+         */
+        SendData: function (time, pass, fail, total_request) {
+            if (fail+pass === total_request-1){
+                io.emit('test-api', {
+                    request:time,
+                    failed: fail,
+                    successful: pass+1
+                });
+            }
+
         }
     }
 })((TestApi = {}) || TestApi);
-
-
-app.get('/test-api', function(req, res) {
-    (async () => {
-        //-- get users' tokens and ids
-        console.log('fetching users\' ids and tokens');
-        let promises = [];
-        promises.push(new Promise((resolve) => {
-            request.post(`${API_URL}/auth/login`, {
-                json: true,
-                form: user
-            }, (err, res, body) => {
-                if (err) {
-                    console.log(err);
-
-                    return;
-                }
-                console.log(body);
-                resolve(body.access_token);
-            });
-        }));
-        promises = await Promise.all(promises);
-        console.log('get token');
-        user.token = promises[0];
-        console.log(user);
-        console.log('get profile');
-        let time = 1;
-        setInterval(async () => {
-            promises = [];
-            //Status now
-            for (let i = 0; i < REQUEST_PER_SECOND; i += 1) {
-                promises.push(new Promise((resolve) => {
-                    request.get(`${API_URL}/api/status_now`, {
-                        json: true,
-                        auth: {
-                            'bearer': user.token
-                        }
-                    }, (err, res, body) => {
-                        if (err) {
-                            console.log(err);
-                            console.dir(err);
-                            return;
-                        }
-                        console.log(`Api ${'status_now'} Lan thu ${time} - request thu ${i}`);
-                        // io.emit('test-api', {a:1});
-                        resolve(body.data);
-                    });
-                }))
-            }
-
-            await Promise.all(promises);
-            time += 1;
-        }, INTERVAL_SECOND);
-    })().then(() => {
-        console.log('Test finished');
-    }).catch((err) => {
-        console.log(err);
-    });
-});
-
-
-
-
-// //Check chat reply
-// for (let i = 0; i < REQUEST_PER_SECOND; i += 1) {
-//     promises.push(new Promise((resolve => {
-//         request.post(`${API_URL}/chat/reply`, {
-//             json: true,
-//             data: {
-//                 message_id: 'ok'
-//             },
-//             auth: {
-//                 'bearer': user.token
-//             }
-//         }, (err, res, body) => {
-//             if (err) {
-//                 console.log(err);
-//                 console.log(err);
-//                 return;
-//             }
-//             console.log(`Api ${'Chat reply is exits'} Lan thu ${time} - request thu ${i}`);
-//             resolve(body.data)
-//         });
-//     })))
-// }
-
-//Check message is exist
-// for (let i = 0; i < REQUEST_PER_SECOND; i += 1) {
-//     promises.push(new Promise((resolve) => {
-//         request.get(`${API_URL}/messages/check_existed/11`, {
-//             json: true,
-//             auth: {
-//                 'bearer': user.token
-//             }
-//         }, (err, res, body) => {
-//             if (err) {
-//                 console.log(err);
-//                 console.dir(err);
-//                 return;
-//             }
-//             console.log(`Api ${'Check message is exits'} Lan thu ${time} - request thu ${i}`);
-//             resolve(body.data);
-//         });
-//     }))
-// }
-//Friend recommend
-// for (let i = 0; i < REQUEST_PER_SECOND; i += 1) {
-//     promises.push(new Promise((resolve) => {
-//         request.get(`${API_URL}/friends/recommendation`, {
-//             json: true,
-//             auth: {
-//                 'bearer': user.token
-//             }
-//         }, (err, res, body) => {
-//             if (err) {
-//                 console.log(err);
-//                 console.dir(err);
-//                 return;
-//             }
-//             console.log(`Api ${'friend_recommend'} Lan thu ${time} - request thu ${i}`);
-//             resolve(body.data);
-//         });
-//     }))
-// }
-
 
 
